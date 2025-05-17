22accc080f747ab8d40fcef7cabd1f82fb8846ef
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-05-17 11:04:59 +0500

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "tailwindcss";
+import ".//App.css";
+import Search from "./components/search.jsx";
+import Spinner from "./components/Spinner.jsx";
+import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "Get",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchTerm, setsearchTerm] = useState("");
+  const [errorMessage, setrerrorMessage] = useState("");
+  const [movieList, setmovieList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [DebounceSearchTerm, setDebounceSearchTerm] = useState(``);
+
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = ``) => {
+    setLoading(true);
+    setrerrorMessage("");
+    /* */
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      const data = await response.json();
+      console.log(data);
+      if (data.response === "false") {
+        setrerrorMessage("error fetching movies: please try again later");
+        setmovieList([]);
+        return;
+      }
+
+      setmovieList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setrerrorMessage("error fetching movies: please try again later");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(DebounceSearchTerm);
+  }, [DebounceSearchTerm]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="bg-[url(hero-bg.png)] w-[95vw] m-0 p-0  h-[100vh] bg-local bg-center bg-cover absolute z-0" />
+        <div className="div wrapper">
+          <header>
+            <img src="./hero.png" alt="hero banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+            </h1>
+            <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+          </header>
+
+          
+
+          <section className="all-movies">
+            <h2 className="mt-[40px]">All Movies </h2>
+
+            {loading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className="text-red-500"> {errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/search.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Search = ({searchTerm,setsearchTerm}) => {
+  return (
+          <div className="search">
+                    <div>
+                              <img src="./search.svg" alt="search" />
+                              <input type="text"
+                              placeholder='Search movies' 
+                              value={searchTerm}
+                              onChange={(e)=>{setsearchTerm(e.target.value)}}/>
+                    </div>
+          </div>
+    
+  )
+}
+
+export default Search
